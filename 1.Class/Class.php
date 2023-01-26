a7503dcf1edbdc065a065d39e5d5be414d73e9c8
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-26 20:29:52 +0700

    Belajar Class

--- 1.Class/Class.php
+++ 1.Class/Class.php
@@ -3,7 +3,7 @@
 
 // Todo Class
 // * Untuk membuat class, kita bisa menggunakan kata kunci class
-// * Penamaan class biasa menggunakan format CamelCase
+// * Penamaan class biasa menggunakan format CamelCase.
 
 
 class Person
