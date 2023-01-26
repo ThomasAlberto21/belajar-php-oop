9d577bcfb84fc3b73a16402ab9668400ca5b9e30
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-26 20:30:16 +0700

    Belajar Object

--- 2.Object/Object.php
+++ 2.Object/Object.php
@@ -2,7 +2,7 @@
 
 // Todo Object
 // * Object adalah hasil instansiasi dari sebuah class
-// * Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama Class dan kurung ()
+// * Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama Class dan kurung ().
 
 
 require_once "Person.php";
