99de4c70bd671256ac37f572a17ca9d6d33593d9
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-26 21:13:07 +0700

    Belajar Function

---
+++ 4.Function/Person.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Todo Function
+// * Selain menambahkan properties, kita juga bisa menambahkan function ke object
+// * Cara dengan mendeklarasikan function tersebut di dalam block class
+// * Sama seperti function biasanya, kita juga bisa menambahkan return value dan parameter
+// * Untuk mengakses function tersebut, kita bisa menggunakan tanda -> dan diikuti dengan nama method nya. Sama seperti mengakses properties
+
+
+
+class Person
+{
+    var string $name;
+    var string $address;
+    var string $country;
+
+    
+    function sayHello(string $name)
+    {
+        echo "Hello $name" . PHP_EOL;
+    }
+}
