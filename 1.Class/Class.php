47188cdd36bc7e41f6c04012101f30b439cfac80
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-26 20:29:12 +0700

    Belajar Class Dan Object

---
+++ 1.Class/Class.php
@@ -0,0 +1,12 @@
+<?php
+
+
+// Todo Class
+// * Untuk membuat class, kita bisa menggunakan kata kunci class
+// * Penamaan class biasa menggunakan format CamelCase
+
+
+class Person
+{
+
+}
---
+++ 2.Object/Object.php
@@ -0,0 +1,12 @@
+<?php
+
+// Todo Object
+// * Object adalah hasil instansiasi dari sebuah class
+// * Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama Class dan kurung ()
+
+
+require_once "Person.php";
+
+$person = new Person();
+var_dump($person);
+
