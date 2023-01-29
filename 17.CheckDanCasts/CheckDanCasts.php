9346740361aac0b7bd746400ae983c34d72dad13
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-01-29 20:11:08 +0700

    Belajar Type Check Dan Casts

---
+++ 17.CheckDanCasts/CheckDanCasts.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "Data.php";
+
+$company = new Company();
+
+sayHelloProgrammer(new Programmer("Thomas"));
+sayHelloProgrammer(new BackendProgrammer("Eko"));
+sayHelloProgrammer(new FrontendProgrammer("Budi"));
---
+++ 17.CheckDanCasts/Data.php
@@ -0,0 +1,51 @@
+<?php
+
+// Todo Type Check & Casts
+// * Sebelumnya kita sudah tau cara melakukan konversi tipe data bukan class
+// * Khusus untuk tipe data object, kita tidak perlu melakukan konversi secara eksplisit
+// * Namun agar aman, sebelum melakukan casts, pastikan kita melakukan type check (pengecekan tipe data), dengan menggunakan kata kunci instanceof
+// * Hasil operator instanceof adalah boolean, true jika tipe data sesuai, false jika tidak sesuai
+
+use BackendProgrammer as GlobalBackendProgrammer;
+use FrontendProgrammer as GlobalFrontendProgrammer;
+use Programmer as GlobalProgrammer;
+
+class Programmer
+{
+
+    public string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+}
+
+
+class BackendProgrammer extends Programmer
+{
+}
+
+
+class FrontendProgrammer extends Programmer
+{
+}
+
+
+class Company
+{
+    public Programmer $programmer;
+}
+
+
+function sayHelloProgrammer(Programmer $programmer)
+{
+
+    if ($programmer instanceof BackendProgrammer) {
+        echo "Hello Backend Programmer $programmer->name" . PHP_EOL;
+    } else if ($programmer instanceof FrontendProgrammer) {
+        echo "Hello Frontend Programmer $programmer->name" . PHP_EOL;
+    } else if ($programmer instanceof Programmer) {
+        echo "Hello Programmer $programmer->name" . PHP_EOL;
+    }
+}
