ea540bf460e0c1b9c50687f3a66f5a66d6d979f6
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-06 10:34:28 +0700

    Belajar Function

--- 4.Function/Function.php
+++ 4.Function/Function.php
@@ -2,7 +2,7 @@
 
 // Todo Function
 // * Selain menambahkan properties, kita juga bisa menambahkan function ke object
-// * Cara dengan mendeklarasikan function tersebut di dalam block class
+// * Cara mendeklarasikan function tersebut harus di dalam block class
 // * Sama seperti function biasanya, kita juga bisa menambahkan return value dan parameter
 // * Untuk mengakses function tersebut, kita bisa menggunakan tanda -> dan diikuti dengan nama method nya. Sama seperti mengakses properties
 
